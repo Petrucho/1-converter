3ce7c6714179261184a7202e35c77e086a7b4661
petr <petrucho@post.cz>
2026-09-16 17:06:14 +0300

    correcting scope variables

--- main.go
+++ main.go
@@ -6,31 +6,31 @@ func main() {
 	const USD_EUR = 1.1
 	const USD_RUB = 100.0
 	const EUR_RUB = USD_EUR * USD_RUB
-	var fromCurrency, toCurrency string
-	var sum float64
+	var global_fromCurrency, global_toCurrency string
+	var global_sum float64
 
-	getInput()
-	calcValues(sum, fromCurrency, toCurrency)
+	global_sum, global_fromCurrency, global_toCurrency = getInput()
+	calcValues(global_sum, global_fromCurrency, global_toCurrency)
 }
 
-func getInput() {
+func getInput() (return_sum float64, return_fromCurrency, return_toCurrency string) {
 
 	fmt.Print("Введите исходящую валюту: ")
-	_, err := fmt.Scan(&fromCurrency)
+	_, err := fmt.Scan(&return_fromCurrency)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
 	fmt.Print("Введите сумму: ")
-	_, err := fmt.Scan(&sum)
+	_, err = fmt.Scan(&return_sum)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
 	fmt.Print("Введите целевую валюту: ")
-	_, err := fmt.Scan(&toCurrency)
+	_, err = fmt.Scan(&return_toCurrency)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -38,7 +38,6 @@ func getInput() {
 	return
 }
 
-func calcValues(sum_p float64, curr1_p string, curr2_p string) float64 {
-	var convertedSum float64
-	return convertedSum
+func calcValues(sum_p float64, curr1_p string, curr2_p string) (return_convertedSum float64) {
+	return
 }
